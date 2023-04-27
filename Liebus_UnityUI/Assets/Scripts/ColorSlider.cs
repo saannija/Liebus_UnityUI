87416fe01c87b463ce6cbbbf29f2a986bdc09fa3
sannija <sofija45642@gmail.com>
2023-04-27 12:12:56 +0300

    Added color/width sliders

---
+++ Liebus_UnityUI/Assets/Scripts/ColorSlider.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class ColorSlider : MonoBehaviour {
+
+	public Slider red;
+    public Slider green;
+    public Slider blue;
+    public Renderer rend;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rend.material.color=new Color(red.value, green.value, blue.value);
+	}
+}
---
+++ Liebus_UnityUI/Assets/Scripts/SizeSlider.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SizeSlider : MonoBehaviour {
+	public GameObject slider;
+    private RectTransform rtransform;
+    public GameObject character;
+	// Use this for initialization
+	public void WidthChanging()
+	{
+		float currentValue=slider.GetComponent<Slider>().value;
+		//character.rtransform.sizeDelta=new Vector2(currentValue,0,2);
+		character.transform.localScale = new Vector2(1F*currentValue,1);
+	}
+}
