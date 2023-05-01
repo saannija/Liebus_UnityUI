d4b7404ba12cc70d80aa10f82e4d2cef69432e7b
saannija <117991126+saannija@users.noreply.github.com>
2023-05-01 14:04:25 +0300

    Size sliders working

--- Liebus_UnityUI/Assets/Scripts/SizeSlider.cs
+++ Liebus_UnityUI/Assets/Scripts/SizeSlider.cs
@@ -1,4 +1,55 @@
-﻿using System.Collections;using System.Collections.Generic;using UnityEngine;using UnityEngine.UI;public class SizeSlider : MonoBehaviour {    public GameObject sliderW;    public GameObject sliderH;    private RectTransform transform;    public GameObject character;	// Use this for initialization	public void WidthChanging()	{        /*		float currentValue=sliderW.GetComponent<Slider>().value;        W = currentValue;		character.transform.localScale = new Vector2(1F*currentValue,H);	*/        float size = sliderW.GetComponent<Slider>().value;        float H = character.transform.localScale.y;        character.transform.localScale = new Vector2(1f * size, H);    }    public void HeightChanging()    {        /*        float currentValue = sliderH.GetComponent<Slider>().value;        H = currentValue;        character.transform.localScale = new Vector2(W,1F*currentValue);    */        float size = sliderH.GetComponent<Slider>().value;        float W = character.transform.localScale.x;        character.transform.localScale = new Vector2(W, 1f * size);        Vector2 pos = character.transform.position;
-        character.transform.position += new Vector3(0.0f, size, 0.0f);
+﻿using UnityEngine;
+using UnityEngine.UI;
 
-    }}
+public class SizeSlider : MonoBehaviour
+{
+    public Slider sliderW;
+    public Slider sliderH;
+    public Image character;
+    private float initialYPos;
+    public float maxHeight = 550f;
+    public float maxWidth = 550f;
+    private float height, width;
+    private Vector2 originalSize;
+
+    void Start()
+    {
+        sliderW.onValueChanged.AddListener(WidthChanging);
+        sliderH.onValueChanged.AddListener(HeightChanging);
+        // Set the image's initial Y position
+        initialYPos = character.rectTransform.anchoredPosition.y;
+        // Store the original size of the image
+        originalSize = character.rectTransform.sizeDelta;
+    }
+
+    public void WidthChanging(float value)
+    {
+        width = value * maxWidth;
+        float H = character.rectTransform.sizeDelta.y;
+        float scale = width / character.rectTransform.sizeDelta.x;
+        character.transform.localScale = new Vector2(scale, H / maxHeight);
+    }
+
+    public void HeightChanging(float value)
+    {
+        float newHeight = value * maxHeight;
+        height = newHeight;
+        float scale = newHeight / character.rectTransform.sizeDelta.y;
+        float newWidth = character.rectTransform.sizeDelta.x * scale;
+        character.rectTransform.sizeDelta = new Vector2(newWidth, newHeight);
+
+        // Move the image up within the frame
+        float newYPos = initialYPos + (newHeight - maxHeight) / 2;
+        character.rectTransform.anchoredPosition = new Vector2(character.rectTransform.anchoredPosition.x, newYPos);
+    }
+
+    public void ResetSize()
+    {
+        // Reset the size of the image to the original size
+        character.rectTransform.sizeDelta = originalSize;
+        character.transform.localScale = Vector2.one;
+        // Reset the slider values
+        sliderW.value = 1f;
+        sliderH.value = 1f;
+    }
+}
